259b6b423c1414866a944c1dd23edb7553ea72d1
lacus6999 <kamulacus@gmail.com>
2022-09-26 21:39:34 +0200

    Bigger duck

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img alt={"kacsa"}
+                <img alt={"kacsa"} width={'100%'}
                      src={"https://external-preview.redd.it/ECYwxWn-Ib1sedqi156Lu7HJ7QJLw4lVET8fJyK43sU.jpg?auto=webp&s=3bb088303d30740e6a6d94cbe3c13c542ef589e5"}/>
             </header>
         </div>
